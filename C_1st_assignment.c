315bca78018f6b485a25643e779fdbb30374fb09
suyeon1 <31675726+suyeon1@users.noreply.github.com>
2017-09-06 11:12:29 +0900

    Add files via upload

---
+++ C_1st_assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{
+ 
+       printf("student id number : 2017301012\n");
+       printf("name : ±è¼ö¿¬\n");
+       printf("mobile phone number : 01051785109\n");
+       printf("GitHub address : suyeon1\n");
+
+       return 0;
+}
